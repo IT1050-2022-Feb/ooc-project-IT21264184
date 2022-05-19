4f53a21380c4bca214e6493cda245af45b12104f
IT21288630 <99311971+IT21288630@users.noreply.github.com>
2022-05-19 09:50:51 +0000

    created files

---
+++ Admin.h
@@ -0,0 +1,26 @@
+#pragma once
+//Contributor IT21288630 Kulathunga.R.G.G.P
+
+#include "User.h"
+#include "Feedback.h"
+#define size 3
+
+
+
+class Admin : public User
+{
+private:
+	string adid;
+	Feedback* fed[size];
+
+public:
+	Admin();
+	Admin(string id, string uname, string pass, int num, string mail);
+	void setAdid(string id);
+	void addFeedback(Feedback* f);
+	string getAdid();
+	string viewFeedback();
+	void addAccount();
+	void removeAccount();
+	~Admin();
+};
