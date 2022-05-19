c6afd43a96dee27af705a0987b0119732af47007
Nilusha Wijeratne <99312650+IT21264184@users.noreply.github.com>
2022-05-19 17:52:36 +0000

    complete

---
+++ Feedback.cpp
@@ -0,0 +1,40 @@
+//Contributor IT21288630 Kulathunga.R.G.G.P
+#include "Feedback.h"
+#include <iostream>
+
+Feedback::Feedback()
+{
+	fid = "";
+	status = "";
+}
+
+Feedback::Feedback(string id, string st)
+{
+	fid = id;
+	status = st;
+}
+
+void Feedback::setFid(string id)
+{
+	fid = id;
+}
+
+void Feedback::setStatus(string st)
+{
+	status = st;
+}
+
+string Feedback::getFid()
+{
+	return fid;
+}
+
+string Feedback::getStatus()
+{
+	return status;
+}
+
+Feedback::~Feedback()
+{
+	cout << "Feedback deleted" << endl;
+}
