74c04e39071346447e7d31e979a29f7003d1ebba
W.A.I.A.BANDARA <99311591+IT21270710@users.noreply.github.com>
2022-05-20 07:04:43 +0000

    Updated 9

--- main.cpp
+++ main.cpp
@@ -19,18 +19,18 @@
 int main()
 {
   User* us1 = new user("Kusun", "hack1234", "0774567833", "Kusun@gmail.com");
-  User* us1 = new user("Nimal", "sanka123", "0776783233", "Nimal@gmail.com");
-  User* us1 = new user("Shanaka", "gihan1234", "0759876342", "Shanaka@gmail.com");
+  User* us2 = new user("Nimal", "sanka123", "0776783233", "Nimal@gmail.com");
+  User* us3 = new user("Shanaka", "gihan1234", "0759876342", "Shanaka@gmail.com");
   
   Employee* em1 = new employee("e1", "1department");
-  Employee* em1 = new employee("e2", "4department");
-  Employee* em1 = new employee("e3", "3department");
+  Employee* em2 = new employee("e2", "4department");
+  Employee* em3 = new employee("e3", "3department");
 
 
   
   Attendance* at1 = new attendance("a1", "11/03/2020", "7.30AM");
-  Attendance* at1 = new attendance("a1", "20/05/2020", "6.30PM");
-  Attendance* at1 = new attendance("a1", "27/07/2020", "7.30AM");
+  Attendance* at2 = new attendance("a1", "20/05/2020", "6.30PM");
+  Attendance* at3 = new attendance("a1", "27/07/2020", "7.30AM");
 
   Feedback* fe1 = new Feedback("f1", "Good");
   Feedback* fe2 = new Feedback("f2", "Nice");
@@ -38,18 +38,19 @@ int main()
 
   Admin* ad1 = new Admin("a1", "Harry", "1234a", 0712354167, "harry@gmail.com");
   Admin* ad2 = new Admin("a2", "Tobi", "4567b", 0712345671, "tobi@gmail.com"); 
+  Admin* ad3 = new Admin("a3", "Ethan", "8524c", 0714532116, "ethan@gmail.com"); 
 
   Event* eve1 = new Event("ev1" , "12/04/2020" , "9.00AM") ;
   Event* eve2 = new Event("ev2" ,"23/05/2020"  , "10.30AM");
   Event* eve3 = new Event("ev3" , "20/06/2020" , "6.30PM" );
 
   Resources* res1= new Resources("re1","Recruiting Process presentation slides");
-  Resources* res1= new Resources("re2","Training and development presentation slides");
-  Resources* res1= new Resources("re3","Career path planing presentation slides");
+  Resources* res2= new Resources("re2","Training and development presentation slides");
+  Resources* res3= new Resources("re3","Career path planing presentation slides");
 
   Manager* mng1= new Manager("mang1","B.M.W.Rathanayake","Accounts Department");
-  Manager* mng1= new Manager("mang1","K.S.Karunarathne","Stores Department");
-  Manager* mng1= new Manager("mang1","K.T.M.Basnayake","Correspondence Department");
+  Manager* mng2= new Manager("mang1","K.S.Karunarathne","Stores Department");
+  Manager* mng3= new Manager("mang1","K.T.M.Basnayake","Correspondence Department");
   
   return 0;
 }
